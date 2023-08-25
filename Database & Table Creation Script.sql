3275ccd947242d956231c157c32ab64727d3596f
rgammelby <rab.gammelby@gmail.com>
2023-08-25 13:10:20 +0200

    creation script, wip ERdiagram & useless word doc uploaded

---
+++ Database & Table Creation Script.sql
@@ -0,0 +1,118 @@
+-- checks if database exists, creates database if not
+IF db_id('Storage') IS NULL
+	USE [master]
+	CREATE DATABASE [Storage]
+
+GO
+
+-- instance table creation; allows user to see incidence of numbers (highest/lowest)
+USE [Storage]
+
+CREATE TABLE Meat (
+	meat_id TINYINT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	meat_type VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Cheese (
+	cheese_id TINYINT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	cheese_type VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Bread (
+	bread_id TINYINT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	bread_type VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Dressing_And_Dip (
+	dad_id TINYINT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	dad_type VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Salad (
+	salad_id TINYINT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	salad_type VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Fruit_And_Veg (
+	fav_id TINYINT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	fav_type VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Ingredients (
+	ingredient_id TINYINT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	meat TINYINT FOREIGN KEY REFERENCES Meat(meat_id),
+	cheese TINYINT FOREIGN KEY REFERENCES Cheese(cheese_id),
+	bread TINYINT FOREIGN KEY REFERENCES Bread(bread_id),
+	dressing_and_dip TINYINT FOREIGN KEY REFERENCES Dressing_And_Dip(dad_id),
+	salad TINYINT FOREIGN KEY REFERENCES Salad(salad_id),
+	fav TINYINT FOREIGN KEY REFERENCES Fruit_And_Veg(fav_id)
+);
+
+CREATE TABLE Water (
+	water_id TINYINT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	amount TINYINT NOT NULL,
+	brand VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Juice (
+	juice_id TINYINT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	flavour VARCHAR(50) NOT NULL,
+	litres DECIMAL (6, 2) NOT NULL
+);
+
+CREATE TABLE Soda (
+	soda_id TINYINT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	flavour VARCHAR(50) NOT NULL,
+	litres DECIMAL (6, 2) NOT NULL
+);
+
+CREATE TABLE Frappe (
+	frappe_id TINYINT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	flavour VARCHAR(50) NOT NULL,
+	litres DECIMAL (6, 2) NOT NULL
+);
+
+CREATE TABLE Milkshake (
+	milkshake_id TINYINT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	flavour VARCHAR(50) NOT NULL,
+	litres DECIMAL (6, 2) NOT NULL
+);
+
+CREATE TABLE Coffee (
+	coffee_id TINYINT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	coffee_type VARCHAR(50) NOT NULL,
+	litres DECIMAL (6, 2) NOT NULL
+);
+
+CREATE TABLE Drinks (
+	drink_id TINYINT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	water TINYINT FOREIGN KEY REFERENCES Water(water_id) NOT NULL,
+	juice TINYINT FOREIGN KEY REFERENCES Juice(juice_id) NOT NULL,
+	soda TINYINT FOREIGN KEY REFERENCES Soda(soda_id) NOT NULL,
+	frappe TINYINT FOREIGN KEY REFERENCES Frappe(frappe_id) NOT NULL,
+	milkshake TINYINT FOREIGN KEY REFERENCES Milkshake(milkshake_id) NOT NULL,
+	coffee TINYINT FOREIGN KEY REFERENCES Coffee(coffee_id) NOT NULL
+);
+
+CREATE TABLE Sides (
+	side_id TINYINT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	amount TINYINT NOT NULL
+);
+
+CREATE TABLE Service_Items (
+	item_id TINYINT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	bags TINYINT NOT NULL,
+	straws TINYINT NOT NULL,
+	trays TINYINT NOT NULL,
+	cups TINYINT NOT NULL,
+	lids TINYINT NOT NULL
+);
+
+CREATE TABLE Orders (
+	order_id TINYINT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+	ingredients TINYINT FOREIGN KEY REFERENCES Ingredients(ingredient_id) NOT NULL,
+	drinks TINYINT FOREIGN KEY REFERENCES Drinks(drink_id) NOT NULL,
+	sides TINYINT FOREIGN KEY REFERENCES Sides(side_id) NOT NULL,
+	service_items TINYINT FOREIGN KEY REFERENCES Service_Items(item_id) NOT NULL
+);
