0fbe5c32f56967b0e9e32db6d860b1f3df9ee0ba
JanickLarsen <janickelev@gmail.com>
2023-08-26 20:38:05 +0200

    Update on Gui

---
+++ mcdonalds Lager/mcdonalds Lager/Gui.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mcdonalds_Lager
+{
+    internal class Gui
+    {
+        private static void Draw<ItemToDraw>(ItemToDraw Draw, int x, int y,ConsoleColor foreGroundColor)
+        {
+            Console.ForegroundColor = foreGroundColor;
+            Console.SetCursorPosition(x, y);
+            Console.Write(Draw);
+            Console.ResetColor();
+        }
+
+
+        public static void DrawBox(int xSize, int ySize, int xStartPosition,int yStartPosition)
+        {
+
+        }
+    }
+}
