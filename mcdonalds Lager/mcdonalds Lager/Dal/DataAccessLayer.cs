eb73027cb0a48e330eda2ecf28267bfbf14d3516
rgammelby <rab.gammelby@gmail.com>
2023-08-28 10:32:20 +0200

    merge conflict resolution

--- mcdonalds Lager/mcdonalds Lager/Dal/DataAccessLayer.cs
+++ mcdonalds Lager/mcdonalds Lager/Dal/DataAccessLayer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,24 @@ namespace mcdonalds_Lager
 {
     internal class DataAccessLayer
     {
+        // returns connection string for linked database
+        static SqlConnection GetConnection()
+        {
+            // not to be included in final push; .gitignore
+            return new SqlConnection(@"Data Source = localhost; Initial Catalog = Storage; Trusted_Connection=true; TrustServerCertificate=true;");
+        }
+
+        // method for freestyle script execution, accepts a script as parameter
+        static void ExecuteScript(string script, SqlConnection s)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(script);
+
+            string sqlQuery = sb.ToString();
+            using (SqlCommand cmd = new SqlCommand(sqlQuery, s))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
     }
 }
