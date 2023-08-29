a5b0de8389f4d0e9472d25b2c9419613ccdf24fe
rgammelby <rab.gammelby@gmail.com>
2023-08-29 16:23:33 +0200

    input validator uploaded

--- mcdonalds Lager/mcdonalds Lager/Dal/DataAccessLayer.cs
+++ mcdonalds Lager/mcdonalds Lager/Dal/DataAccessLayer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using mcdonalds_Lager.Præsentation;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -81,6 +82,107 @@ namespace mcdonalds_Lager
                 }
                 Console.Write("\n");
             }
+        }
+
+        static dynamic Input()
+        {
+            // draws input field at console x, y; change as needed
+            InputField(0, 0);
+
+            // retrives and evalutes input
+            var input = GetInput();
+            var valid = InputValidator(input);
+
+            //displays error if valid evalutes to false
+            if (valid == false)
+                InputError();
+
+            // if valid, confirms input validity, mainly for testing
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nInput is valid. ");
+                Console.ResetColor();
+            }
+
+            var inputI = 0;
+            var inputF = 0.0;
+            bool inputIsInt;
+
+            // writes here also mainly for testing
+            try
+            {
+                // if able to convert input to int, return int
+                inputI = Convert.ToInt32(input);
+                Console.WriteLine("int input: " + input);
+                inputIsInt = true;
+            }
+            catch
+            {
+                // if not able to convert input to int, return float
+                inputF = float.Parse(input);
+                Console.WriteLine("Input is in float format. ");
+                inputIsInt = false;
+            }
+
+            // also mainly for testing, remove as needed
+            Console.ReadLine();
+
+            // returns input in int or float format
+            if (inputIsInt)
+                return inputI;
+
+            else 
+                return inputF;
+        }
+
+        // gets input
+        static string GetInput()
+        {
+            var input = Console.ReadLine();
+
+            return input;
+        }
+
+        // draws input field at console position x, y
+        static void InputField(int x, int y)
+        {
+            box box = new box();
+            ConsoleDraw.DrawBox(30, 2, x, y);
+            Console.SetCursorPosition(x + 1, y + 1);
+        }
+
+        // outputs error if input not valid
+        static void InputError()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nInvalid input. Make sure input is a number, containing no letters, punctuation or whitespace.");
+            Console.ResetColor();
+        }
+
+        // evalutes input validity true/false
+        static bool InputValidator(string input)
+        {
+            var valid = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                // if each character of string input is NOT a letter, punctuation or whitespace
+                if (!char.IsLetter(input[i]) && !char.IsPunctuation(input[i]) && !char.IsWhiteSpace(input[i]))
+                    valid = true;
+
+                // if a character of string input IS punctuation, but is full stop or comma
+                else if (char.IsPunctuation(input[i]) && (input[i] == '.' || input[i] == ','))
+                    valid = true;
+
+                else
+                {
+                    valid = false;
+                    break;
+                }
+            }
+
+            return valid;
         }
     }
 }
