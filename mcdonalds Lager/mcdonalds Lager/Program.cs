5faedc264b630b24e497602aa30fe3f06d11771e
rgammelby <rab.gammelby@gmail.com>
2023-08-29 10:24:49 +0200

    split Select() method into GetData() and Print()

--- mcdonalds Lager/mcdonalds Lager/Program.cs
+++ mcdonalds Lager/mcdonalds Lager/Program.cs
@@ -13,12 +13,12 @@ namespace mcdonalds_Lager
     {
         public static void Main()
         {
-            //SqlConnection s = DataAccessLayer.GetConnection();
-            //s.Open();
+            SqlConnection s = DataAccessLayer.GetConnection();
+            s.Open();
             
             Console.SetWindowSize(170,45);
-            UserController.Controller();
-            //DataAccessLayer.Select(s, "Coffee");
+            //UserController.Controller();
+            DataAccessLayer.Print(s, "Coffee");
             Console.ReadLine();
         }
     }
