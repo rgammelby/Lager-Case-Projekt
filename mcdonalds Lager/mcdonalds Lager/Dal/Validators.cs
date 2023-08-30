1a31c9ec270d0a0a27b1630393d62c1c749b0e64
rgammelby <rab.gammelby@gmail.com>
2023-08-30 10:34:34 +0200

    input validator updated to not accept ^ as valid input

--- mcdonalds Lager/mcdonalds Lager/Dal/Validators.cs
+++ mcdonalds Lager/mcdonalds Lager/Dal/Validators.cs
@@ -9,14 +9,14 @@ namespace mcdonalds_Lager.Dal
     internal class Validators
     {
         // evalutes input validity true/false
-        public static bool InputValidator(string input)
+        static bool InputValidator(string input)
         {
             var valid = false;
 
             for (int i = 0; i < input.Length; i++)
             {
                 // if each character of string input is NOT a letter, punctuation or whitespace
-                if (!char.IsLetter(input[i]) && !char.IsPunctuation(input[i]) && !char.IsWhiteSpace(input[i]))
+                if (char.IsDigit(input[i]))
                     valid = true;
 
                 // if a character of string input IS punctuation, but is full stop or comma
